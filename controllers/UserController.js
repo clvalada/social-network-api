903fe86028511827f3c6ebc167111f0b14ec4b71
clvalada <clvalada@gmail.com>
2023-12-06 12:37:48 -0500

    updated userController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,8 +12,78 @@ const UserController = {
     }
   },
 
-  // Add other controller methods for handling specific user operations
+  getUserById: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  createUser: async (req, res) => {
+    try {
+      const { username } = req.body;
+
+      // Check if the username is provided
+      if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+      }
+
+      const newUser = new User({ username });
+      const savedUser = await newUser.save();
+
+      res.status(201).json(savedUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  updateUser: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const { username } = req.body;
+
+      // Check if the username is provided
+      if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(userId, { username }, { new: true });
+
+      if (!updatedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  deleteUser: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+
+      const deletedUser = await User.findByIdAndDelete(userId);
+
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  // Add more controller methods as needed
 };
 
 module.exports = UserController;
-
