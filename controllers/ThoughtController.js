8cf04588153523e88417ac4fc1224dcd3b6901f5
clvalada <clvalada@gmail.com>
2023-12-07 11:19:57 -0500

    added routes to ThoughtController.js

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -12,6 +12,77 @@ const ThoughtController = {
     }
   },
 
+  getThoughtById: async (req, res) => {
+    try {
+      const thoughtId = req.params.thoughtId;
+      const thought = await Thought.findById(thoughtId);
+
+      if (!thought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+
+      res.json(thought);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  createThought: async (req, res) => {
+    try {
+      const { thoughtText, username } = req.body;
+
+      // Check if required fields are provided
+      if (!thoughtText || !username) {
+        return res.status(400).json({ error: 'Thought text and username are required' });
+      }
+
+      const newThought = new Thought({ thoughtText, username });
+      const savedThought = await newThought.save();
+
+      res.status(201).json(savedThought);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  updateThought: async (req, res) => {
+    try {
+      const thoughtId = req.params.thoughtId;
+      const { thoughtText } = req.body;
+
+      // Check if thought text is provided
+      if (!thoughtText) {
+        return res.status(400).json({ error: 'Thought text is required' });
+      }
+
+      const updatedThought = await Thought.findByIdAndUpdate(thoughtId, { thoughtText }, { new: true });
+
+      if (!updatedThought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+
+      res.json(updatedThought);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  deleteThought: async (req, res) => {
+    try {
+      const thoughtId = req.params.thoughtId;
+
+      const deletedThought = await Thought.findByIdAndDelete(thoughtId);
+
+      if (!deletedThought) {
+        return res.status(404).json({ error: 'Thought not found' });
+      }
+
+      res.json({ message: 'Thought deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
 };
 
 module.exports = ThoughtController;
